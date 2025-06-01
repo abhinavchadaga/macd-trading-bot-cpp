6ad07eb08685b05665471fd3568a4422b4e722c3
abhinavchadaga <abhinav.chadaga@utexas.edu>
2025-06-01 02:26:25 +0000

    chore: clean up variable substitution in configure.sh

--- configure.sh
+++ configure.sh
@@ -2,14 +2,14 @@
 
 buildType=$1
 
-if [ -z "$1" ]; then
+if [[ -z $1 ]]; then
 	buildType="Debug"
 fi
 
-if [ ! -d "build/${buildType}" ]; then
-	mkdir -p build/"$buildType"
+if [[ ! -d "build/${buildType}" ]]; then
+	mkdir -p build/"${buildType}"
 fi
 
-echo "using build type $buildType"
-cmake -S . -B build/"$buildType" -GNinja -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_TOOLCHAIN_FILE=cmake/linux-gcc.cmake
-ln -s build/"${buildType}"/compile_commands.json compile_commands.json
+echo "using build type ${buildType}"
+cmake -S . -B build/"${buildType}" -GNinja -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_TOOLCHAIN_FILE=cmake/linux-gcc.cmake
+ln -sf build/"${buildType}"/compile_commands.json compile_commands.json
